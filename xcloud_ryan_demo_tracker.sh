aee64c79f1a907e938f528958cb4d09b5f29cd99
Ryan Burgert <burgert@google.com>
2025-04-06 08:00:38 +0000

    XCloud Script

---
+++ xcloud_ryan_demo_tracker.sh
@@ -0,0 +1,9 @@
+#Meant to run on XCloud
+
+export HF_HUB_OFFLINE=1 
+
+rclone sync --progress /home/jupyter/CleanCode/CloudSync/home_cache ~/.cache
+bash ~/CleanCode/Management/sync_projects.bash
+bash ryan_demo_tracker.sh
+
+bash ./ryan_demo_tracker.sh
