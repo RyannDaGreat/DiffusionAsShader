8b1f8fe31526b8bc661fd49a9ce2d15a00407f25
Ryan Burgert <burgert@google.com>
2025-04-21 21:39:29 +0000

    Fix Makecolumns

--- source/datasets/youtube/DaS/Vanilla/make_columns.py
+++ source/datasets/youtube/DaS/Vanilla/make_columns.py
@@ -31,17 +31,19 @@ rp.load_files(
     num_threads=100,
 )
 
+root = rp.get_path_parent(__file__)
+
 prompt            = [x.prompt.replace('\n',' ')          for x in samples]
 videos            = [x.video_480p49_path                 for x in samples]
 trackings         = [x.video_dasTrackvid_path            for x in samples]
 counter_trackings = [x.cogxCounterVideo_dasTrackvid_path for x in samples]
 counter_videos    = [x.cogxCounterVideo_path             for x in samples]
 
-prompt_path            = rp.save_file_lines(prompt           , "prompt.txt"           )
-videos_path            = rp.save_file_lines(videos           , "videos.txt"           )
-trackings_path         = rp.save_file_lines(trackings        , "trackings.txt"        )
-counter_trackings_path = rp.save_file_lines(counter_trackings, "counter_trackings.txt")
-counter_videos_path    = rp.save_file_lines(counter_videos   , "counter_videos.txt"   )
+prompt_path            = rp.save_file_lines(prompt           , rp.path_join(root, "prompt.txt"           ))
+videos_path            = rp.save_file_lines(videos           , rp.path_join(root, "videos.txt"           ))
+trackings_path         = rp.save_file_lines(trackings        , rp.path_join(root, "trackings.txt"        ))
+counter_trackings_path = rp.save_file_lines(counter_trackings, rp.path_join(root, "counter_trackings.txt"))
+counter_videos_path    = rp.save_file_lines(counter_videos   , rp.path_join(root, "counter_videos.txt"   ))
 
 print(rp.fansi_highlight_path(rp.get_absolute_path(prompt_path           )))
 print(rp.fansi_highlight_path(rp.get_absolute_path(videos_path           )))
