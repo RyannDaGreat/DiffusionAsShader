f97f2277401ea857ff4525e74a1a4ed327af118d
Ryan Burgert <burgert@google.com>
2025-04-27 01:22:54 +0000

    Use 2500 sampels

--- source/datasets/youtube/DaS/Vanilla/make_columns.py
+++ source/datasets/youtube/DaS/Vanilla/make_columns.py
@@ -16,8 +16,8 @@ from source.datasets.youtube.youtube_dataset import ProcessedYoutubeDataset
 dataset = ProcessedYoutubeDataset()
 
 # Right now just using 1000 samples so I can test the code and make sure it runs
-# samples = rp.gather(dataset, range(2500))
-samples = rp.gather(dataset, range(10))
+samples = rp.gather(dataset, range(2500))
+# samples = rp.gather(dataset, range(10))
 
 def prepare_sample(sample):
     if not rp.path_exists(sample.video_480p49_path):
