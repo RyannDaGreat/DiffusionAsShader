b4b919b4c22c9f458baf0b917ba641371cd35bb6
Ryan Burgert <burgert@google.com>
2025-06-05 05:00:29 +0000

    populator.py: Reverse Tracks Generator

--- source/datasets/youtube/populator.py
+++ source/datasets/youtube/populator.py
@@ -1,3 +1,4 @@
+from rp import *
 import os
 import subprocess
 import sys
@@ -115,6 +116,49 @@ def process_random_sample():
 
 
 @rp.globalize_locals
+def process_sample_reversetrack(index):
+    '''
+    EXAMPLE:
+        >>> sample_dir='/home/jupyter/CleanCode/Datasets/Vids/Raw_Feb28/Processed_April7/---5pJOK6Kw_358439294_365642146'
+        ... with SetCurrentDirectoryTemporarily(sample_dir):
+        ...     v  =rp.load_video('video_480p49.mp4'                                                                   , use_cache=True)              
+        ...     rv =rp.load_video('reverse_video.mp4'                                                                  , use_cache=True)               
+        ...     tv =rp.load_video('video.mp4__DiffusionAsShaderCondition/tracking_video.mp4'                           , use_cache=True)                                                              
+        ...     rtv=rp.load_video('reverse_video.mp4__DiffusionAsShaderCondition/tracking_video.mp4'                   , use_cache=True)                                                              
+        ... 
+        ... hcv = rp.horizontally_concatenated_videos(v,tv)
+        ... hcrv = rp.horizontally_concatenated_videos(rv,rtv)
+        ... vcv=vertically_concatenated_videos(hcv,hcrv)
+        ... save_video_mp4(vcv,'Comparison.mp4',framerate=20)
+    '''
+    setup()
+
+    filename           = "video.mp4"
+    firstlast_filename = "firstLastInterp_Jack2000.mp4"
+
+    reverse_firstlast_filename = "reverse_firstLastInterp_Jack2000.mp4"
+    reverse_filename           = "reverse_video.mp4"
+
+    sample = dataset[index]
+    sample.download()
+
+    rp.pretty_print(sample)
+
+    with rp.SetCurrentDirectoryTemporarily(sample.path):
+        if file_exists(reverse_filename):
+            #Don't process twice
+            return sample
+
+        rp.save_video_mp4(rp.resize_list(rp.resize_images(rp.load_video(filename          )[::-1], size=(480, 720)), 49), reverse_filename          )
+        rp.save_video_mp4(rp.resize_list(rp.resize_images(rp.load_video(firstlast_filename)[::-1], size=(480, 720)), 49), reverse_firstlast_filename)
+
+        run_diffusion_as_shader_tracker(reverse_filename          , gpu)
+        run_diffusion_as_shader_tracker(reverse_firstlast_filename, gpu)
+
+    return sample
+
+
+@rp.globalize_locals
 def main(_gpu: int = None):
 
     global gpu
@@ -148,6 +192,51 @@ def launch_all():
     yaml=rp.tmuxp_create_session_yaml(
         {f"GPU{i}": f"{sys.executable} {__file__} main {i}" for i in rp.get_all_gpu_ids()},
         session_name="Datagen",
+    )
+    rp.tmuxp_launch_session_from_yaml(yaml) 
+
+
+@rp.globalize_locals
+def main_reversetracks(_gpu: int = None):
+
+    setup()
+
+    global gpu
+    gpu = _gpu
+
+    index = int(gpu)
+
+    NUM_WORKERS = 8
+
+    while True:
+        try:
+            sample = dataset[index]
+            process_sample_reversetrack(index)
+            sample.upload()
+            index = index + NUM_WORKERS
+            rp.fansi_print(index,'green green bold on dark blue underlined')
+            # sample.clear_local() #Eh, if we fill 1TB we're doing a good job lol
+        except Exception:
+            rp.sleep(2)
+            rp.print_stack_trace()
+
+def launch_all_reversetracks():
+    """
+    TO LAUNCH:
+        bash ~/CleanCode/Management/sync_projects.bash
+        python ~/CleanCode/Github/CogvideX-Interpolation-Mar23:MotionPrompting/cogvideox_interpolation/datasets/youtube/populator.py launch_all
+    """
+
+    rp.tmux_kill_session('DatagenReversetracks')
+
+    os.system(
+        "rclone sync --progress /home/jupyter/CleanCode/CloudSync/home_cache ~/.cache"
+    )  # bash
+
+    """ python populator.py launch_all """
+    yaml=rp.tmuxp_create_session_yaml(
+        {f"GPU{i}": f"{sys.executable} {__file__} main_reversetracks {i}" for i in rp.get_all_gpu_ids()},
+        session_name="DatagenReversetracks",
     )
     rp.tmuxp_launch_session_from_yaml(yaml) 
 
